1305bcf5c90ff18dd750751ccea450533dd25693
Manpreetsingh88 <manpreetsingh88.msk@gmail.com>
2025-03-29 20:17:31 -0400

    Fix MIDDLEWARE typo

--- health_project/health_project/settings.py
+++ health_project/health_project/settings.py
@@ -45,14 +45,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← this is for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  # ← this should NOT be merged
+
+
 
 ]
 
